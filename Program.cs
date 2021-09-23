bfcfbae56254ab7b987904c1765838c6411ed449
panbin <1764183867@qq.com>
2021-09-23 13:21:08 +0800

    Update project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Net.Mime;
+
+namespace miniRenderer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+
+   
+}
