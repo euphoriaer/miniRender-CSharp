559f6d556e66da594ae157afc594ae733ba55299
panbin <1764183867@qq.com>
2021-09-27 18:17:20 +0800

    Add Matri4x4

--- Matrix4X4.cs
+++ Matrix4X4.cs
@@ -1,113 +1,169 @@
-﻿namespace miniRenderer
+﻿using System;
+using System.Numerics;
+
+namespace miniRenderer
 {
     internal class Matrix4X4
     {
-        public float m11;
-        public float m12;
-        public float m13;
-        public float m14;
+        public double m11;
+        public double m12;
+        public double m13;
+        public double m14;
 
-        public float m21;
-        public float m22;
-        public float m23;
-        public float m24;
+        public double m21;
+        public double m22;
+        public double m23;
+        public double m24;
 
-        public float m31;
-        public float m32;
-        public float m33;
-        public float m34;
+        public double m31;
+        public double m32;
+        public double m33;
+        public double m34;
 
-        public float m41;
-        public float m42;
-        public float m43;
-        public float m44;
+        public double m41;
+        public double m42;
+        public double m43;
+        public double m44;
 
-        public Matrix4X4(float m11 = 0f
-        , float m12 = 0f
-        , float m13 = 0f
-        , float m14 = 0f
-
-        , float m21 = 0f
-        , float m22 = 0f
-        , float m23 = 0f
-        , float m24 = 0f
-
-        , float m31 = 0f
-        , float m32 = 0f
-        , float m33 = 0f
-        , float m34 = 0f
-
-        , float m41 = 0f
-        , float m42 = 0f
-        , float m43 = 0f
-        , float m44 = 0f)
-        {
-            m11 = m11;
-            m12 = m12;
-            m13 = m13;
-            m14 = m14;
-            ;
-            m21 = m21;
-            m22 = m22;
-            m23 = m23;
-            m24 = m24;
-            ;
-            m31 = m31;
-            m32 = m32;
-            m33 = m33;
-            m34 = m34;
-            ;
-            m41 = m41;
-            m42 = m42;
-            m43 = m43;
-            m44 = m44;
-        }
         /// <summary>
-        /// 矩阵叉乘
+        /// 构造秩为3的矩阵
         /// </summary>
-        /// <param name="matrix1"></param>
-        /// <param name="matrix2"></param>
-        /// <returns></returns>
-        public static Matrix4X4 MultiplyCross(Matrix4X4 matrix1, Matrix4X4 matrix2)
+        /// <param name="vector3"></param>
+        /// <param name=""></param>
+        public Matrix4X4(Vector3 row1, Vector3 row2, Vector3 row3
+            )
         {
-            //按行计算
-            float rem11 = SixNumMult(matrix1.m11, matrix1.m12, matrix1.m13, matrix1.m14, matrix2.m11, matrix2.m21, matrix2.m31, matrix2.m41);
-            float rem12 = SixNumMult(matrix1.m11, matrix1.m12, matrix1.m13, matrix1.m14, matrix2.m12, matrix2.m22, matrix2.m32, matrix2.m42);
-            float rem13 = SixNumMult(matrix1.m11, matrix1.m12, matrix1.m13, matrix1.m14, matrix2.m13, matrix2.m23, matrix2.m33, matrix2.m43);
-            float rem14 = SixNumMult(matrix1.m11, matrix1.m12, matrix1.m13, matrix1.m14, matrix2.m14, matrix2.m24, matrix2.m34, matrix2.m44);
-            float rem21 = SixNumMult(matrix1.m21, matrix1.m22, matrix1.m23, matrix1.m24, matrix2.m11, matrix2.m21, matrix2.m31, matrix2.m41);
-            float rem22 = SixNumMult(matrix1.m21, matrix1.m22, matrix1.m23, matrix1.m24, matrix2.m12, matrix2.m22, matrix2.m32, matrix2.m42);
-            float rem23 = SixNumMult(matrix1.m21, matrix1.m22, matrix1.m23, matrix1.m24, matrix2.m13, matrix2.m23, matrix2.m33, matrix2.m43);
-            float rem24 = SixNumMult(matrix1.m21, matrix1.m22, matrix1.m23, matrix1.m24, matrix2.m14, matrix2.m24, matrix2.m34, matrix2.m44);
-            float rem31 = SixNumMult(matrix1.m31, matrix1.m32, matrix1.m33, matrix1.m34, matrix2.m11, matrix2.m21, matrix2.m31, matrix2.m41);
-            float rem32 = SixNumMult(matrix1.m31, matrix1.m32, matrix1.m33, matrix1.m34, matrix2.m12, matrix2.m22, matrix2.m32, matrix2.m42);
-            float rem33 = SixNumMult(matrix1.m31, matrix1.m32, matrix1.m33, matrix1.m34, matrix2.m13, matrix2.m23, matrix2.m33, matrix2.m43);
-            float rem34 = SixNumMult(matrix1.m31, matrix1.m32, matrix1.m33, matrix1.m34, matrix2.m14, matrix2.m24, matrix2.m34, matrix2.m44);
-            float rem41 = SixNumMult(matrix1.m41, matrix1.m42, matrix1.m43, matrix1.m44, matrix2.m11, matrix2.m21, matrix2.m31, matrix2.m41);
-            float rem42 = SixNumMult(matrix1.m41, matrix1.m42, matrix1.m43, matrix1.m44, matrix2.m12, matrix2.m22, matrix2.m32, matrix2.m42);
-            float rem43 = SixNumMult(matrix1.m41, matrix1.m42, matrix1.m43, matrix1.m44, matrix2.m13, matrix2.m23, matrix2.m33, matrix2.m43);
-            float rem44 = SixNumMult(matrix1.m41, matrix1.m42, matrix1.m43, matrix1.m44, matrix2.m14, matrix2.m24, matrix2.m34, matrix2.m44);
-            Matrix4X4 retu = new Matrix4X4(rem11, rem12, rem13, rem14, rem21, rem22, rem23, rem24, rem31, rem32, rem33, rem34, rem41, rem42, rem43, rem44);
-            return retu;
+            this.m11 = row1.X;
+            this.m12 = row1.Y;
+            this.m13 = row1.Z;
+            this.m14 = 0;
+            this.m21 = row2.X;
+            this.m22 = row2.Y;
+            this.m23 = row2.Z;
+            this.m24 = 0;
+            this.m31 = row3.X;
+            this.m32 = row3.Y;
+            this.m33 = row3.Z;
+            this.m34 = 0;
+            this.m41 = 0;
+            this.m42 = 0;
+            this.m43 = 0;
+            this.m44 = 0;
         }
 
-        private static float SixNumMult(float m11, float m12, float m13, float m14, float m21, float m22, float m23,
-            float m24)
+        public Matrix4X4(double m11 = 0f
+        , double m12 = 0f
+        , double m13 = 0f
+        , double m14 = 0f
+
+        , double m21 = 0f
+        , double m22 = 0f
+        , double m23 = 0f
+        , double m24 = 0f
+
+        , double m31 = 0f
+        , double m32 = 0f
+        , double m33 = 0f
+        , double m34 = 0f
+
+        , double m41 = 0f
+        , double m42 = 0f
+        , double m43 = 0f
+        , double m44 = 0f)
+        {
+            this.m11 = m11;
+            this.m12 = m12;
+            this.m13 = m13;
+            this.m14 = m14;
+            this.m21 = m21;
+            this.m22 = m22;
+            this.m23 = m23;
+            this.m24 = m24;
+            this.m31 = m31;
+            this.m32 = m32;
+            this.m33 = m33;
+            this.m34 = m34;
+            this.m41 = m41;
+            this.m42 = m42;
+            this.m43 = m43;
+            this.m44 = m44;
+        }
+
+        private static double SixNumMult(double m11, double m12, double m13, double m14, double m21, double m22, double m23,
+            double m24)
         {
             return m11 * m21 + m12 * m22 + m13 * m23 + m14 * m24;
         }
 
-        /// <summary>
-        /// 矩阵点乘
-        /// </summary>
-        /// <param name="matrix1"></param>
-        /// <param name="matrix2"></param>
-        /// <returns></returns>
-        public static float Dot(Matrix4X4 matrix1, Matrix4X4 matrix2)
+        private static double SixNumMult(double m11, double m12, double m13, double m21, double m22, double m23)
         {
-            float retu = 0f;
+            return m11 * m21 + m12 * m22 + m13 * m23;
+        }
 
+        public static Vector3 operator *(Matrix4X4 matrix1, Vector3 vector)
+        {
+            double rem11 = SixNumMult(matrix1.m11, matrix1.m12, matrix1.m13, vector.X, vector.Y, vector.Z);
+            double rem12 = SixNumMult(matrix1.m21, matrix1.m22, matrix1.m23, vector.X, vector.Y, vector.Z);
+            double rem13 = SixNumMult(matrix1.m31, matrix1.m32, matrix1.m33, vector.X, vector.Y, vector.Z);
+            return new Vector3((float)rem11, (float)rem12, (float)rem13);
+        }
+
+        public static Matrix4X4 operator *(Matrix4X4 matrix1, Matrix4X4 matrix2)
+        {
+            //按行计算
+            double rem11 = SixNumMult(matrix1.m11, matrix1.m12, matrix1.m13, matrix1.m14, matrix2.m11, matrix2.m21, matrix2.m31, matrix2.m41);
+            double rem12 = SixNumMult(matrix1.m11, matrix1.m12, matrix1.m13, matrix1.m14, matrix2.m12, matrix2.m22, matrix2.m32, matrix2.m42);
+            double rem13 = SixNumMult(matrix1.m11, matrix1.m12, matrix1.m13, matrix1.m14, matrix2.m13, matrix2.m23, matrix2.m33, matrix2.m43);
+            double rem14 = SixNumMult(matrix1.m11, matrix1.m12, matrix1.m13, matrix1.m14, matrix2.m14, matrix2.m24, matrix2.m34, matrix2.m44);
+            double rem21 = SixNumMult(matrix1.m21, matrix1.m22, matrix1.m23, matrix1.m24, matrix2.m11, matrix2.m21, matrix2.m31, matrix2.m41);
+            double rem22 = SixNumMult(matrix1.m21, matrix1.m22, matrix1.m23, matrix1.m24, matrix2.m12, matrix2.m22, matrix2.m32, matrix2.m42);
+            double rem23 = SixNumMult(matrix1.m21, matrix1.m22, matrix1.m23, matrix1.m24, matrix2.m13, matrix2.m23, matrix2.m33, matrix2.m43);
+            double rem24 = SixNumMult(matrix1.m21, matrix1.m22, matrix1.m23, matrix1.m24, matrix2.m14, matrix2.m24, matrix2.m34, matrix2.m44);
+            double rem31 = SixNumMult(matrix1.m31, matrix1.m32, matrix1.m33, matrix1.m34, matrix2.m11, matrix2.m21, matrix2.m31, matrix2.m41);
+            double rem32 = SixNumMult(matrix1.m31, matrix1.m32, matrix1.m33, matrix1.m34, matrix2.m12, matrix2.m22, matrix2.m32, matrix2.m42);
+            double rem33 = SixNumMult(matrix1.m31, matrix1.m32, matrix1.m33, matrix1.m34, matrix2.m13, matrix2.m23, matrix2.m33, matrix2.m43);
+            double rem34 = SixNumMult(matrix1.m31, matrix1.m32, matrix1.m33, matrix1.m34, matrix2.m14, matrix2.m24, matrix2.m34, matrix2.m44);
+            double rem41 = SixNumMult(matrix1.m41, matrix1.m42, matrix1.m43, matrix1.m44, matrix2.m11, matrix2.m21, matrix2.m31, matrix2.m41);
+            double rem42 = SixNumMult(matrix1.m41, matrix1.m42, matrix1.m43, matrix1.m44, matrix2.m12, matrix2.m22, matrix2.m32, matrix2.m42);
+            double rem43 = SixNumMult(matrix1.m41, matrix1.m42, matrix1.m43, matrix1.m44, matrix2.m13, matrix2.m23, matrix2.m33, matrix2.m43);
+            double rem44 = SixNumMult(matrix1.m41, matrix1.m42, matrix1.m43, matrix1.m44, matrix2.m14, matrix2.m24, matrix2.m34, matrix2.m44);
+            Matrix4X4 retu = new Matrix4X4(rem11, rem12, rem13, rem14, rem21, rem22, rem23, rem24, rem31, rem32, rem33, rem34, rem41, rem42, rem43, rem44);
             return retu;
+        }
+
+        public static Matrix4X4 operator +(Matrix4X4 matrix1, Matrix4X4 matrix2)
+        {
+            //按行计算
+            double rem11 = matrix1.m11 + matrix2.m11;
+            double rem12 = matrix1.m12 + matrix2.m12;
+            double rem13 = matrix1.m13 + matrix2.m13;
+            double rem14 = matrix1.m14 + matrix2.m14;
+            double rem21 = matrix1.m21 + matrix2.m21;
+            double rem22 = matrix1.m22 + matrix2.m22;
+            double rem23 = matrix1.m23 + matrix2.m23;
+            double rem24 = matrix1.m24 + matrix2.m24;
+            double rem31 = matrix1.m31 + matrix2.m31;
+            double rem32 = matrix1.m32 + matrix2.m32;
+            double rem33 = matrix1.m33 + matrix2.m33;
+            double rem34 = matrix1.m34 + matrix2.m34;
+            double rem41 = matrix1.m41 + matrix2.m41;
+            double rem42 = matrix1.m42 + matrix2.m42;
+            double rem43 = matrix1.m43 + matrix2.m43;
+            double rem44 = matrix1.m44 + matrix2.m44;
+            Matrix4X4 retu = new Matrix4X4(rem11, rem12, rem13, rem14, rem21, rem22, rem23, rem24, rem31, rem32, rem33, rem34, rem41, rem42, rem43, rem44);
+            return retu;
+        }
+
+        public override string ToString()
+        {
+            string re = String.Format(
+                           "{0} {1} {2} {3}\n" +
+                           "{4} {5} {6} {7}\n" +
+                           "{8} {9} {10} {11}\n" +
+                           "{12} {13} {14} {15}\n", m11.ToString(),
+                 m12.ToString(), m13.ToString(), m14.ToString(), m21.ToString(), m22.ToString(), m23.ToString(), m24.ToString(), m31.ToString(), m32.ToString(), m33.ToString(), m34.ToString(), m41.ToString(), m42.ToString(), m43.ToString(), m44.ToString());
+
+            return re;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,24 +1,54 @@
 ﻿using System;
-using System.Drawing;
-using System.Net.Mime;
 using System.Numerics;
 
 namespace miniRenderer
 {
-    class Program
+    internal class Program
     {
         private float a = 1f;
         private float b = 1f;
-   
 
-         void Main(string[] args)
-         {
+        public static void Main(string[] args)
+        {
+            
             Console.WriteLine("Hello World!");
-      
-       
-         }
+            float a = 1f, b = 2f;
+            Math.Sqrt(b);
+            var pi = Math.Acos(-1);
+            var sin30 = Math.Sin((30.0 / 180.0 * Math.PI));
+            Vector3 v = new Vector3(1.0f, 2.0f, 3.0f);
+            Vector3 w = new Vector3(1.0f, 0.0f, 0.0f);
+            Console.WriteLine(v + w);
+            Console.WriteLine(v * 2);
+            Console.WriteLine(2 * v);
 
+            Console.WriteLine("矩阵测试!");
+            Matrix4X4 i = new Matrix4X4(1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f);
+            Matrix4X4 j = new Matrix4X4(2.0f, 3.0f, 1.0f, 4.0f, 6.0f, 5.0f, 9.0f, 7.0f, 8.0f);
+            Console.WriteLine(i);
+
+            //point
+            Vector3 pointPa = new Vector3(1, 1, 1);
+            Console.WriteLine("逆时针旋转45°  数学上是  +45° 所以计算弧度制\n");
+            double Rad45 = (45.0 / 180.0 * Math.PI);
+
+            //rotate
+            Matrix4X4 rotate = new Matrix4X4(
+                Math.Cos(Rad45), -Math.Sin(Rad45), 0,
+                Math.Sin(Rad45), Math.Cos(Rad45), 0,
+                0, 0, 1
+            );
+            //trans
+            Matrix4X4 transform = new Matrix4X4(
+                1, 0, 1,
+                0, 1, 2,
+                0, 0, 1
+            );
+            Matrix4X4 homogeneous = new Matrix4X4(new Vector3((float)Math.Cos(Rad45), -(float)Math.Sin(Rad45), 1), new Vector3((float)Math.Sin(Rad45), (float)Math.Cos(Rad45), 2), new Vector3(0, 0, 1));
+
+            var fin = homogeneous * pointPa;
+            //计算
+            Console.WriteLine(fin);
+        }
     }
-
-   
 }
